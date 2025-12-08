1b62849df054e9d25f43a79e631ba2085a1f618d
Screammyzz <diamondjirapat@gmail.com>
2025-12-08 18:32:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
     MONGODB_URI = os.getenv('MONGODB_URI')
@@ -10,16 +11,14 @@ class Config:
     COMMAND_PREFIX = os.getenv('PREFIX', '!')
     INTENTS_ALL = True
 
-    # LAVALINK_HOST = os.getenv('LAVALINK_HOST', 'localhost')
-    # LAVALINK_PORT = int(os.getenv('LAVALINK_PORT', 2333))
-    # LAVALINK_PASSWORD = os.getenv('LAVALINK_PASSWORD', 'youshallnotpass')
-    # LAVALINK_SSL = os.getenv('LAVALINK_SSL', 'false').lower() == 'true'
+    # Lavalink Configuration
+    LAVALINK_URI = os.getenv('LAVALINK_URI', 'http://localhost:8090')
+    LAVALINK_PASSWORD = os.getenv('LAVALINK_PASSWORD', 'youshallnotpass')
 
-    # LASTFM_API_KEY = os.getenv('LASTFM_API_KEY')
-    # LASTFM_API_SECRET = os.getenv('LASTFM_API_SECRET')
-    
+    # Last.fm
+    LASTFM_API_KEY = os.getenv('LASTFM_API_KEY')
+    LASTFM_API_SECRET = os.getenv('LASTFM_API_SECRET')
 
-    
     @classmethod
     def validate(cls):
         if not cls.DISCORD_TOKEN:
